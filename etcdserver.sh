17daf18c324bdd110c0b227cc875d5fcb59ca8cb
U-INOVATE\raghuvamsi <kgrvamsi@yahoo.com>
2015-08-26 18:19:28 +0530

    Fix etcdserver script

--- etcdserver.sh
+++ etcdserver.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-echo "ETCD_SERVER=$ETCD_SERVER" >/opt/etcdserver
+echo "ETCD_SERVER=$ETCD_SERVER" >>/opt/etcdserver
 
-echo "ETCD_PREFIX=$ETCD_PREFIX" >/opt/etcdserver
+echo "ETCD_PREFIX=$ETCD_PREFIX" >>/opt/etcdserver
 
 cat /opt/etcdserver >>/opt/envvars
 
